a9665ec4443cd1f2b7a2de6cb535b0a3f083fa97
ryan_fitra <ryanfitraperdana@gmail.com>
2026-06-03 14:19:32 +0700

    update route gabungan

--- routes/api.php
+++ routes/api.php
@@ -150,11 +150,11 @@ Route::middleware(['auth:sanctum', 'check.api.access'])->group(function () {
         // GET detail_pendidikan_formal
         Route::get('detail-pendidikan-formal', [SatuDataController::class, 'get_detail_pendidikan_formal'])->name('api.dosen.get-detail-pendidikan-formal');
         // GET detail_penelitian
-        Route::get('detail_penelitian', [SatuDataController::class, 'get_detail_penelitian'])->name('api.dosen.get-detail-penelitian');
+        Route::get('detail-penelitian', [SatuDataController::class, 'get_detail_penelitian'])->name('api.dosen.get-detail-penelitian');
         // GET detail_penugasan_dosen
-        Route::get('detail_penugasan_dosen', [SatuDataController::class, 'get_detail_penugasan_dosen'])->name('api.dosen.get-detail-penugasan-dosen');
+        Route::get('detail-penugasan-dosen', [SatuDataController::class, 'get_detail_penugasan_dosen'])->name('api.dosen.get-detail-penugasan-dosen');
         // GET dokumen
-        Route::get('dokumen', [SatuDataController::class, 'get_detail_ajuan_sertifikasi_dosen'])->name('api.dosen.get-dokumen');
+        Route::get('dokumen', [SatuDataController::class, 'get_dokumen'])->name('api.dosen.get-dokumen');
         // GET dokumen_paten
         Route::get('dokumen-paten', [SatuDataController::class, 'get_detail_paten'])->name('api.dosen.get-dokumen-paten');
         // GET dokumen_pendidikan_formal
@@ -162,12 +162,47 @@ Route::middleware(['auth:sanctum', 'check.api.access'])->group(function () {
         // GET dokumen_penelitian
         Route::get('dokumen-penelitian', [SatuDataController::class, 'get_detail_penelitian'])->name('api.dosen.get-dokumen-penelitian');
         // GET dokumen_publikasi
-        Route::get('dokumen-publikasi', [SatuDataController::class, 'get_detail_publikasi'])->name('api.dosen.get-dokumen-publikasi');
+        Route::get('dokumen-publikasi', [SatuDataController::class, 'get_dokumen_publikasi'])->name('api.dosen.get-dokumen-publikasi');
         // GET dosen_pembimbing
         Route::get('dosen-pembimbing', [SatuDataController::class, 'get_dosen_pembimbing'])->name('api.dosen.get-dosen-pembimbing');
         // GET dosen_pengajar_kelas_kuliah
         Route::get('dosen-pengajar-kelas-kuliah', [SatuDataController::class, 'get_dosen_pengajar_kelas_kuliah'])->name('api.dosen.get-dosen-pengajar-kelas-kuliah');
-
+        Route::get('sister-list-ajuan_jabatan_fungsional', [SatuDataController::class, 'get_sister_list_ajuan_jabatan_fungsional'])->name('api.dosen.get_sister_list_ajuan_jabatan_fungsional');
+        Route::get('sister-list-ajuan_kepangkatan', [SatuDataController::class, 'get_sister_list_ajuan_kepangkatan'])->name('api.dosen.get_sister_list_ajuan_kepangkatan');
+        Route::get('sister-list-ajuan_nilai_tes', [SatuDataController::class, 'get_sister_list_ajuan_nilai_tes'])->name('api.dosen.get_sister_list_ajuan_nilai_tes');
+        Route::get('sister-list-ajuan_pendidikan_formal', [SatuDataController::class, 'get_sister_list_ajuan_pendidikan_formal'])->name('api.dosen.get_sister_list_ajuan_pendidikan_formal');
+        Route::get('sister-list-ajuan_sertifikasi_dosen', [SatuDataController::class, 'get_sister_list_ajuan_sertifikasi_dosen'])->name('api.dosen.get_sister_list_ajuan_sertifikasi_dosen');
+        Route::get('sister-list-anggota_profesi', [SatuDataController::class, 'get_sister_list_anggota_profesi'])->name('api.dosen.get_sister_list_anggota_profesi');
+        Route::get('sister-list-bahan_ajar', [SatuDataController::class, 'get_sister_list_bahan_ajar'])->name('api.dosen.get_sister_list_bahan_ajar');
+        Route::get('sister-list-beasiswa', [SatuDataController::class, 'get_sister_list_beasiswa'])->name('api.dosen.get_sister_list_beasiswa');
+        Route::get('sister-list-bimbingan_mahasiswa', [SatuDataController::class, 'get_sister_list_bimbingan_mahasiswa'])->name('api.dosen.get_sister_list_bimbingan_mahasiswa');
+        Route::get('sister-list-datasering', [SatuDataController::class, 'get_sister_list_datasering'])->name('api.dosen.get_sister_list_datasering');
+        Route::get('sister-list-diklat', [SatuDataController::class, 'get_sister_list_diklat'])->name('api.dosen.get_sister_list_diklat');
+        Route::get('sister-list-inpassing', [SatuDataController::class, 'get_sister_list_inpassing'])->name('api.dosen.get_sister_list_inpassing');
+        Route::get('sister-list-jabatan_fungsional', [SatuDataController::class, 'get_sister_list_jabatan_fungsional'])->name('api.dosen.get_sister_list_jabatan_fungsional');
+        Route::get('sister-list-jabatan_struktural', [SatuDataController::class, 'get_sister_list_jabatan_struktural'])->name('api.dosen.get_sister_list_jabatan_struktural');
+        Route::get('sister-list-kekayaan_intelektual', [SatuDataController::class, 'get_sister_list_kekayaan_intelektual'])->name('api.dosen.get_sister_list_kekayaan_intelektual');
+        Route::get('sister-list-kepangkatan', [SatuDataController::class, 'get_sister_list_kepangkatan'])->name('api.dosen.get_sister_list_kepangkatan');
+        Route::get('sister-list-kesejahteraan', [SatuDataController::class, 'get_sister_list_kesejahteraan'])->name('api.dosen.get_sister_list_kesejahteraan');
+        Route::get('sister-list-nilai_tes', [SatuDataController::class, 'get_sister_list_nilai_tes'])->name('api.dosen.get_sister_list_nilai_tes');
+        Route::get('sister-list-orasi_ilmiah', [SatuDataController::class, 'get_sister_list_orasi_ilmiah'])->name('api.dosen.get_sister_list_orasi_ilmiah');
+        Route::get('sister-list-pembicara', [SatuDataController::class, 'get_sister_list_pembicara'])->name('api.dosen.get_sister_list_pembicara');
+        Route::get('sister-list-penelitian', [SatuDataController::class, 'get_sister_list_penelitian'])->name('api.dosen.get_sister_list_penelitian');
+        Route::get('sister-list-pendidikan_formal', [SatuDataController::class, 'get_sister_list_pendidikan_formal'])->name('api.dosen.get_sister_list_pendidikan_formal');
+        Route::get('sister-list-pengabdian', [SatuDataController::class, 'get_sister_list_pengabdian'])->name('api.dosen.get_sister_list_pengabdian');
+        Route::get('sister-list-pengajaran', [SatuDataController::class, 'get_sister_list_pengajaran'])->name('api.dosen.get_sister_list_pengajaran');
+        Route::get('sister-list-pengelola_jurnal', [SatuDataController::class, 'get_sister_list_pengelola_jurnal'])->name('api.dosen.get_sister_list_pengelola_jurnal');
+        Route::get('sister-list-penghargaan', [SatuDataController::class, 'get_sister_list_penghargaan'])->name('api.dosen.get_sister_list_penghargaan');
+        Route::get('sister-list-pengujian_mahasiswa', [SatuDataController::class, 'get_sister_list_pengujian_mahasiswa'])->name('api.dosen.get_sister_list_pengujian_mahasiswa');
+        Route::get('sister-list-penugasan', [SatuDataController::class, 'get_sister_list_penugasan'])->name('api.dosen.get_sister_list_penugasan');
+        Route::get('sister-list-penunjang_lain', [SatuDataController::class, 'get_sister_list_penunjang_lain'])->name('api.dosen.get_sister_list_penunjang_lain');
+        Route::get('sister-list-publikasi', [SatuDataController::class, 'get_sister_list_publikasi'])->name('api.dosen.get_sister_list_publikasi');
+        Route::get('sister-list-riwayat-pekerjaan', [SatuDataController::class, 'get_sister_list_riwayat_pekerjaan'])->name('api.dosen.get_sister_list_riwayat_pekerjaan');
+        Route::get('sister-list-sertifikasi-dosen', [SatuDataController::class, 'get_sister_list_sertifikasi_dosen'])->name('api.dosen.get_sister_list_sertifikasi_dosen');
+        Route::get('sister-list-sertifikasi-profesi', [SatuDataController::class, 'get_sister_list_sertifikasi_profesi'])->name('api.dosen.get_sister_list_sertifikasi_profesi');
+        Route::get('sister-list-tugas-tambahan', [SatuDataController::class, 'get_sister_list_tugas_tambahan'])->name('api.dosen.get_sister_list_tugas_tambahan');
+        Route::get('sister-list-tunjangan', [SatuDataController::class, 'get_sister_list_tunjangan'])->name('api.dosen.get_sister_list_tunjangan');
+        Route::get('sister-list-visiting-scientist', [SatuDataController::class, 'get_sister_list_visiting_scientist'])->name('api.dosen.get_sister_list_visiting_scientist');
 
 
     //PERKULIAHAN
@@ -179,9 +214,38 @@ Route::middleware(['auth:sanctum', 'check.api.access'])->group(function () {
         Route::get('detail-mata-kuliah', [SatuDataController::class, 'get_detail_mata_kuliah'])->name('api.perkuliahan.get-detail-mata-kuliah');
         // GET detail_nilai_perkuliahan
         Route::get('detail-nilai-perkuliahan', [SatuDataController::class, 'get_detail_nilai_perkuliahan'])->name('api.perkuliahan.get-detail-nilai-perkuliahan');
+
+
+        Route::get('kebutuhan-khusus', [SatuDataController::class, 'kebutuhan_khusus'])->name('api.referensi.kebutuhan-khusus');
+        Route::get('kelompok-bidang', [SatuDataController::class, 'kelompok_bidang'])->name('api.referensi.kelompok-bidang');
+        Route::get('kelompok-mata-kuliah', [SatuDataController::class, 'kelompok_mata_kuliah'])->name('api.referensi.kelompok-mata-kuliah');
+        Route::get('konversi-kampus-merdeka', [SatuDataController::class, 'konversi_kampus_merdeka'])->name('api.referensi.konversi-kampus-merdeka');
+        Route::get('krs-mahasiswa', [SatuDataController::class, 'krs_mahasiswa'])->name('api.referensi.krs-mahasiswa');
+        Route::get('lembaga-pengangkat', [SatuDataController::class, 'lembaga_pengangkat'])->name('api.referensi.lembaga-pengangkat');
+        Route::get('level-wilayah', [SatuDataController::class, 'level_wilayah'])->name('api.referensi.level-wilayah');
+        Route::get('list-aktivitas-mahasiswa', [SatuDataController::class, 'list_aktivitas_mahasiswa'])->name('api.referensi.list-aktivitas-mahasiswa');
+        Route::get('list-aktivitas-mahasiswa-copy', [SatuDataController::class, 'list_aktivitas_mahasiswa_copy'])->name('api.referensi.list-aktivitas-mahasiswa-copy');
+        Route::get('list-anggota-aktivitas-mahasiswa', [SatuDataController::class, 'list_anggota_aktivitas_mahasiswa'])->name('api.referensi.list-anggota-aktivitas-mahasiswa');
+        Route::get('list-bimbing-mahasiswa', [SatuDataController::class, 'list_bimbing_mahasiswa'])->name('api.referensi.list-bimbing-mahasiswa');
+        Route::get('list-dosen', [SatuDataController::class, 'list_dosen'])->name('api.referensi.list-dosen');
+        Route::get('list-kelas-kuliah', [SatuDataController::class, 'list_kelas_kuliah'])->name('api.referensi.list-kelas-kuliah');
+        Route::get('list-kurikulum', [SatuDataController::class, 'list_kurikulum'])->name('api.referensi.list-kurikulum');
+        Route::get('list-mahasiswa', [SatuDataController::class, 'list_mahasiswa'])->name('api.referensi.list-mahasiswa');
+        Route::get('list-mahasiswa-lulus-do', [SatuDataController::class, 'list_mahasiswa_lulus_do'])->name('api.referensi.list-mahasiswa-lulus-do');
+        Route::get('list-mahasiswa-lulus-do-bak', [SatuDataController::class, 'list_mahasiswa_lulus_do_bak'])->name('api.referensi.list-mahasiswa-lulus-do-bak');
+        Route::get('list-nilai-perkuliahan', [SatuDataController::class, 'list_nilai_perkuliahan'])->name('api.referensi.list-nilai-perkuliahan');
+        Route::get('list-pendidikan-formal', [SatuDataController::class, 'list_pendidikan_formal'])->name('api.referensi.list-pendidikan-formal');
+        Route::get('list-penelitian', [SatuDataController::class, 'list_penelitian'])->name('api.referensi.list-penelitian');
+        Route::get('list-penugasan-dosen', [SatuDataController::class, 'list_penugasan_dosen'])->name('api.referensi.list-penugasan-dosen');
+        Route::get('list-periode-perkuliahan', [SatuDataController::class, 'list_periode_perkuliahan'])->name('api.referensi.list-periode-perkuliahan');
+        Route::get('list-prestasi-mahasiswa', [SatuDataController::class, 'list_prestasi_mahasiswa'])->name('api.referensi.list-prestasi-mahasiswa');
+        Route::get('list-riwayat-pendidikan-mahasiswa', [SatuDataController::class, 'list_riwayat_pendidikan_mahasiswa'])->name('api.referensi.list-riwayat-pendidikan-mahasiswa');
+        Route::get('list-skala-nilai-prodi', [SatuDataController::class, 'list_skala_nilai_prodi'])->name('api.referensi.list-skala-nilai-prodi');
+        Route::get('list-substansi-kuliah', [SatuDataController::class, 'list_substansi_kuliah'])->name('api.referensi.list-substansi-kuliah');
+        Route::get('list-uji-mahasiswa', [SatuDataController::class, 'list_uji_mahasiswa'])->name('api.referensi.list-uji-mahasiswa');
+        Route::get('loop-inserts', [SatuDataController::class, 'loop_inserts'])->name('api.referensi.loop-inserts');
+        Route::get('lppm-penelitian-dosen', [SatuDataController::class, 'lppm_penelitian_dosen'])->name('api.referensi.lppm-penelitian-dosen');
+        Route::get('lppm-penelitian-scopus-dosens', [SatuDataController::class, 'lppm_penelitian_scopus_dosens'])->name('api.referensi.lppm-penelitian-scopus-dosens');
+        Route::get('mahasiswa-lulus-do', [SatuDataController::class, 'mahasiswa_lulus_do'])->name('api.referensi.mahasiswa-lulus-do');
     });
-
-
-
-
 });
