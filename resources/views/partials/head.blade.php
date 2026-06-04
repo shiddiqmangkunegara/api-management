aa0af99d88529427f163cb9842827ca263d3adf3
ryan_fitra <ryanfitraperdana@gmail.com>
2026-06-04 11:16:20 +0700

    update logo dan url endpoint

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,11 +1,12 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'Laravel' }}</title>
+<title>{{ $title ?? 'API PDUNSRI' }}</title>
+
+<link rel="icon" type="image/png" href="{{ asset('images/unsri.svg') }}">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
